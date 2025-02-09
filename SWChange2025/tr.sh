20af8902a90ea2291180e988b141806917ef9ed5
UCAS-ChenZC <czc_142229@163.com>
2025-02-09 20:24:16 +0800

    change code

---
+++ SWChange2025/tr.sh
@@ -0,0 +1,10 @@
+# python main_cd.py --project_name 'test1219Res+FADConv_2_batch8_12_19_16_49_(1)'
+#python main_cd.py --project_name 'Swin+CBAM#' --net_G 'SwinChangeFormer' --pretrain '/home/solid/下载/swin_tiny_patch4_window7_224.pth'
+python main_cd.py --project_name 'test0114_NewSwin_256128SegFusion_Bitch2(1)' --batch_size 2
+python main_cd.py --project_name 'test0114_NewSwin_256128SegFusion_Bitch2(2)' --batch_size 2
+python main_cd.py --project_name 'test0114_NewSwin_256128SegFusion_Bitch2(3)' --batch_size 2
+python main_cd.py --project_name 'test0114_NewSwin_256128SegFusion_Bitch2(4)' --batch_size 2
+python main_cd.py --project_name 'test0114_NewSwin_256128SegFusion_Bitch2(5)' --batch_size 2
+python main_cd.py --project_name 'test0114_NewSwin_256128SegFusion_Bitch2(6)' --batch_size 2
+python main_cd.py --project_name 'test0114_NewSwin_256128SegFusion_Bitch2(7)' --batch_size 2
+python main_cd.py --project_name 'test0114_NewSwin_256128SegFusion_Bitch2(8)' --batch_size 2
